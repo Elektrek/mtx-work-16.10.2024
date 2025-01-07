af1776bb743ebcd3936089ffb741cd791aa58313
Maksim_Shramko <maxramko@gmail.com>
2025-01-07 16:17:27 +0300

    Time to work.. Creating class in header

--- matrix.hpp
+++ matrix.hpp
@@ -1,11 +1,30 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
+
 #include <cstddef>
 
 
-void destroyMemory(int** t, size_t m);
-int** createMatrix(size_t m, size_t n);
-void input(int** t, size_t m, size_t n);
-void out(const int* const* t, size_t m, size_t n);
+class Matrix
+{
+  int** t_ = nullptr;
+  size_t m_ = 0;
+  size_t n_ = 0;
+
+  int** createMtx();
+  void rmMtx();
+
+public:
+  Matrix(size_t m, size_t n);
+  Matrix(matrix &mtx);
+
+  ~Matrix();
+
+  size_t getRows() const;
+  size_t getCols() const;
+
+  void inMtx();
+  void outMtx() const;
+  int** changeSize(size_t m, size_t n);
+};
 
 #endif
